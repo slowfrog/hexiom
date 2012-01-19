9473693bf67349d946c25519990963afaf5b403a
SlowFrog <laurentvaucher@gmail.com>
2012-01-19 17:19:23 +0100

    Added scripts for profiling + profiling info

---
+++ profile.sh
@@ -0,0 +1,1 @@
+java -javaagent:profile.jar -cp build/main/classes com.slowfrog.hexiom.Main level01.txt level02.txt level03.txt level04.txt level05.txt level06.txt level07.txt level08.txt level09.txt level10.txt level11.txt level12.txt level13.txt level14.txt level15.txt level16.txt level17.txt level18.txt level19.txt level20.txt
